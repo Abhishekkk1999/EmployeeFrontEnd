f4a42ab62d065ec9ca544307e894fff24fbbd3b1
Abhishek Kumar <79020815+Abhishekkk1999@users.noreply.github.com>
2023-03-23 22:35:39 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { FooterComponent } from './footer/footer.component';
-import { ProductCardComponent } from './product-card/product-card.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
+    NavbarComponent,
     HeroComponent,
     FooterComponent,
-    ProductCardComponent
+    CardComponent
   ],
   imports: [
     BrowserModule,
